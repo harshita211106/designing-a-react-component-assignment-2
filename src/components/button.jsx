d63a1a2187fb7c48ca83fbb998b7d408e7a12f82
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2025-03-07 11:14:34 +0530

    task completed

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,10 @@
 // write button card here
+
+import React from "react";
+
+const Button=({onClick,text})=>{
+    return <button className="view-button" onClick={onclick}>{text}</button>
+
+};
+
+export default Button;
